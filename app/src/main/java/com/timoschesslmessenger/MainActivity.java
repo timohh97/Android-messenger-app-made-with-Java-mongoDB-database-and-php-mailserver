8130d792f686cc6839a2e10ab63767462b8ab0e2
Timo <timo.schessl@gmx.de>
2020-05-16 11:48:58 +0200

    UI works, connection with database works, working on PostTask

---
+++ app/src/main/java/com/timoschesslmessenger/MainActivity.java
@@ -0,0 +1,254 @@
+package com.timoschesslmessenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+import org.bson.Document;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MainActivity extends AppCompatActivity {
+
+    JSONArray globalJsonArray = new JSONArray();
+    public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
+            Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DownloadTask downloadTask = new DownloadTask();
+
+        try {
+
+            downloadTask.execute("https://timoschessl-mail.netlify.app/.netlify/functions/server");
+
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public void createNewAccount(View view) throws JSONException {
+        Log.i("Test:","Button clicked");
+        EditText editTextUsername = findViewById(R.id.editText5);
+        EditText editTextEmail = findViewById(R.id.editText6);
+        EditText editTextPassword = findViewById(R.id.editText7);
+        EditText editTextRepeat = findViewById(R.id.editText8);
+
+        String username = editTextUsername.getText().toString();
+        String email = editTextEmail.getText().toString();
+        String password = editTextPassword.getText().toString();
+        String repeat = editTextRepeat.getText().toString();
+
+        Log.i("Test:",username);
+
+        if(username.length()>3)
+        {
+            if(!checkIfUsernameExists(username))
+            {
+
+
+                if(validate(email))
+                {
+
+                    if(password.length()>5)
+                    {
+                        if(password.equals(repeat))
+                        {
+                            Toast toast = Toast.makeText(this,"Creating a new account....",Toast.LENGTH_LONG);
+                            toast.show();
+
+                            PostTask postTask = new PostTask();
+
+                            try {
+
+                                postTask.execute("https://timoschessl-mail.netlify.app/.netlify/functions/server");
+
+                            }
+                            catch(Exception e)
+                            {
+                                e.printStackTrace();
+                            }
+
+                        }
+                        else
+                        {
+                            Toast toast = Toast.makeText(this,"The passwords dont match!",Toast.LENGTH_LONG);
+                            toast.show();
+                        }
+                    }
+                    else
+                    {
+                        Toast toast = Toast.makeText(this,"Please enter at least 6 characters for the password!",Toast.LENGTH_LONG);
+                        toast.show();
+                    }
+                }
+                else
+                {
+                    Toast toast = Toast.makeText(this,"The email is not valid!",Toast.LENGTH_SHORT);
+                    toast.show();
+
+                }
+            }
+            else
+            {
+                Toast toast = Toast.makeText(this,"This username already exists!",Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        }
+        else
+        {
+            Toast toast = Toast.makeText(this,"Please enter at least 4 characters for the username!",Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+
+    }
+
+
+    public boolean checkIfUsernameExists(String username) throws JSONException {
+
+        Log.i("Global",globalJsonArray.toString());
+
+        for(int i=0;i<globalJsonArray.length() ;i++)
+        {
+            Log.i("Objects:",globalJsonArray.getJSONObject(i).toString());
+            JSONObject userJsonObject = globalJsonArray.getJSONObject(i);
+
+            String usernameOfUserInDatabase = userJsonObject.getString("username");
+            Log.i("Usernames:",usernameOfUserInDatabase);
+
+            if(usernameOfUserInDatabase.equals(username))
+            {
+                return true;
+            }
+
+        }
+
+        return false;
+
+    }
+
+
+    public class DownloadTask extends AsyncTask<String,Void,String> {
+
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            JSONArray jsonArray = new JSONArray();
+
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            String result = "";
+
+            try {
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = urlConnection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                int data = inputStreamReader.read();
+
+                while (data != -1) {
+                    char current = (char) data;
+                    result += current;
+                    data = inputStreamReader.read();
+                }
+
+                jsonArray = new JSONArray(result);
+                globalJsonArray= new JSONArray(result);
+                Log.i("Json:", jsonArray.toString());
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return jsonArray.toString();
+        }
+
+
+    }
+
+    public static boolean validate(String emailStr) {
+        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(emailStr);
+        return matcher.find();
+    }
+
+    public class PostTask extends AsyncTask<String,Void,String> {
+
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            JSONArray jsonArray = new JSONArray();
+
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            String result = "";
+
+            try {
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = urlConnection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                int data = inputStreamReader.read();
+
+                while (data != -1) {
+                    char current = (char) data;
+                    result += current;
+                    data = inputStreamReader.read();
+                }
+
+                jsonArray = new JSONArray(result);
+                globalJsonArray= new JSONArray(result);
+                Log.i("Json:", jsonArray.toString());
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return jsonArray.toString();
+        }
+
+
+    }
+
+
+
+
+}
